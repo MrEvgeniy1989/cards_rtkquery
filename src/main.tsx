1a8735cc28b8d87e40daf3ad5124f9ac1370d324
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2023-06-28 17:33:36 +0300

    исправил ошибку автодобавления импортов

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,17 @@ import './styles/index.scss'
 import { StrictMode } from 'react'
 
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import { App } from './App.tsx'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/home" element={<div>Home</div>} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 )
